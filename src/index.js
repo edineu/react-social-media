51bd0d305b2383fc228aa347f235d90fabbf8538
Ed Hansen <ed@neubra.dk>
2021-01-19 13:24:07 +0100

    array destucturing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const greeting = <div>Hello React</div>;
-const rootNode = document.getElementById("root");
+function App() {
+  const [inputValue, setInputValue] = React.useState("");
 
-ReactDom.render(greeting, rootNode);
+  // Use Array Destructuring
+  // const inputValue = inputState[0];
+  // const setInputValue = inputState[1]; // function to update the input value
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value); // set input value function
+  }
+
+  return (
+    <div id="app">
+      <input onChange={handleInputChange} />
+      <p>{inputValue}</p>
+    </div>
+  );
+}
+
+const rootNote = document.getElementById("root");
+ReactDom.render(<App />, rootNote);
